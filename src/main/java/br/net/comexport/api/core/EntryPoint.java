72a270e6faf1346dc03940f88acc84a21df14a4d
Diego D. Mucciolo <diego.mucciolo@intelie.com.br>
2021-02-28 16:59:47 -0300

    added apiinfo

--- src/main/java/br/net/comexport/api/core/EntryPoint.java
+++ src/main/java/br/net/comexport/api/core/EntryPoint.java
@@ -3,8 +3,10 @@ package br.net.comexport.api.core;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -32,14 +34,17 @@ public class EntryPoint {
                            .build(),
                    new ResponseMessageBuilder()
                            .code(BAD_REQUEST.value())
-                           .message(
-                                   "Sintaxe mal formada ou requisição inválida")
+                           .message("Sintaxe mal formada ou requisição inválida")
                            .build(),
                    new ResponseMessageBuilder()
                            .code(INTERNAL_SERVER_ERROR.value())
-                           .message(
-                                   "Erro interno do servidor.")
+                           .message("Erro interno do servidor.")
                            .build());
+
+    private static final ApiInfo API_INFO = new ApiInfoBuilder()
+            .title("Comexport order service")
+            .version("v1")
+            .build();
 
     public static void main(final String[] args) {
         run(EntryPoint.class, args);
@@ -47,6 +52,7 @@ public class EntryPoint {
 
     @Bean
     public Docket buildSwaggerDocket() {
+
         return new Docket(SWAGGER_2)
                 .select()
                 .apis(basePackage(API_PACKAGE))
@@ -55,6 +61,7 @@ public class EntryPoint {
                 .useDefaultResponseMessages(false)
                 .globalResponseMessage(GET, RESPONSE_MESSAGE_LIST)
                 .globalResponseMessage(PUT, RESPONSE_MESSAGE_LIST)
-                .globalResponseMessage(DELETE, RESPONSE_MESSAGE_LIST);
+                .globalResponseMessage(DELETE, RESPONSE_MESSAGE_LIST)
+                .apiInfo(API_INFO);
     }
 }
