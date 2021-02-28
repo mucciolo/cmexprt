a11fd9eda9f5c75687a4d544a2ab5a1fa8e36186
Diego D. Mucciolo <diego.mucciolo@intelie.com.br>
2021-02-28 20:23:26 -0300

    simplified UserController

--- src/main/java/br/net/comexport/api/core/controller/UserController.java
+++ src/main/java/br/net/comexport/api/core/controller/UserController.java
@@ -8,23 +8,20 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
-import java.util.Date;
 
-import static br.net.comexport.api.core.util.ControllerUtils.deleteFromRepositoryById;
-import static br.net.comexport.api.core.util.ControllerUtils.findInRepositoryById;
-import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
+import static br.net.comexport.api.core.util.ControllerUtils.*;
 import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping(value = "user")
 @Api(tags = {"User API"})
-public final class UserController {
+public class UserController {
 
-    private static final String MSG_EMAIL_CHANGE_NOT_ALLOWED = "Changing e-mail is not allowed after creation.";
+    private static final ExampleMatcher LIST_EXAMPLE_MATCHER = ExampleMatcher.matching()
 
     @Autowired
     private UserRepository userRepository;
@@ -35,18 +32,10 @@ public final class UserController {
     }
 
     @GetMapping
-    public Page<User> list(@RequestParam(required = false) final String name,
-                           @RequestParam(required = false) final String email,
-                           @RequestParam(required = false) @DateTimeFormat(pattern = User.DATE_PATTERN) final Date birthdate,
+    public Page<User> list(final User user,
                            @RequestParam(defaultValue = "0") final int pageNum,
                            @RequestParam(defaultValue = "10") final int pageSize) {
-
-        final Example<User> userExample = Example.of(new User(name, email, birthdate),
-                                                     ExampleMatcher.matching()
-                                                                   .withIgnoreCase()
-                                                                   .withMatcher(User.NAME, contains()));
-
-        return userRepository.findAll(userExample, PageRequest.of(pageNum, pageSize));
+        return userRepository.findAll(Example.of(user, LIST_EXAMPLE_MATCHER), PageRequest.of(pageNum, pageSize));
     }
 
     @PutMapping
@@ -56,14 +45,9 @@ public final class UserController {
     }
 
     @PutMapping("/{id}")
+    @Transactional
     public User update(@PathVariable final Long id, @RequestBody @Valid final User updatedUser) {
-
-        if (!findInRepositoryById(userRepository, id).getEmail().equals(updatedUser.getEmail()))
-            throw new IllegalArgumentException(MSG_EMAIL_CHANGE_NOT_ALLOWED);
-        else
-            updatedUser.setId(id);
-
-        return userRepository.save(updatedUser);
+        return updateRepositoryById(userRepository, id, updatedUser);
     }
 
     @DeleteMapping("/{id}")
