acb914f9a5a69e8d4dd49596b6d85cea8dce145d
Diego D. Mucciolo <diego.mucciolo@intelie.com.br>
2021-02-28 22:32:12 -0300

    refactored ProductController to BaseController

--- src/main/java/br/net/comexport/api/core/controller/ProductController.java
+++ src/main/java/br/net/comexport/api/core/controller/ProductController.java
@@ -3,48 +3,16 @@ package br.net.comexport.api.core.controller;
 import br.net.comexport.api.core.entity.Product;
 import br.net.comexport.api.core.repository.ProductRepository;
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import javax.transaction.Transactional;
-import javax.validation.Valid;
-import java.util.List;
-
-import static br.net.comexport.api.core.util.ControllerUtils.*;
-import static org.springframework.http.HttpStatus.CREATED;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("product")
 @Api(tags = {"Product API"})
-public class ProductController {
+public class ProductController extends BaseController<Product, Long, ProductRepository> {
 
-    @Autowired
-    private ProductRepository productRepository;
-
-    @GetMapping("/{id}")
-    public Product findById(@PathVariable final Long id) {
-        return findInRepositoryById(productRepository, id);
-    }
-
-    @GetMapping
-    public List<Product> listAll() {
-        return productRepository.findAll();
-    }
-
-    @PutMapping
-    @ResponseStatus(CREATED)
-    public Product create(@RequestBody @Valid final Product newProduct) {
-        return productRepository.save(newProduct);
-    }
-
-    @PutMapping("/{id}")
-    @Transactional
-    public Product update(@PathVariable final Long id, @RequestBody @Valid final Product updatedProduct) {
-        return updateRepositoryById(productRepository, id, updatedProduct);
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable final Long id) {
-        return deleteFromRepositoryById(productRepository, id);
+    public ProductController() {
+        super(ExampleMatcher.matching());
     }
 }
